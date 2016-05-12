15cee488578bbbda3837f81212cb12c6b82eedc7
ryan.veitch <ryan.r.veitch@gmail.com>
2016-05-12 17:21:47 -0500

    Updated quantcast tracking pixel
    - Replaced quantcast tracking pixel with new code.
    - Cleaned up code syntax

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FCC Tracking Code Manager
 Places Quantcast and Google Analytics tracking code in the footer of each site.
+
+- AreaVoices GA Tracking Code: `UA-778232-45`
--- fcc-tracking-code-manager.php
+++ fcc-tracking-code-manager.php
@@ -1,37 +1,63 @@
 <?php
-  /**
-	* Plugin Name: FCC Tracking Code Manager
-	* Plugin URI: https://github.com/openfcci/fcc-tracking-code-manager
-	* Description: A plugin that places quantcast and google analytics tracking code in the footer of each blog on the install (as long as the blog's theme uses the 'wp_footer' action).
-	* Version: 1.1
-	* Author: Jed Limke
-	* Author URI: http://www.fccinteractive.com
-	* Site Wide Only: true
-	* Network: true
-	*/
+/**
+* Plugin Name: FCC Tracking Code Manager
+* Plugin URI: https://github.com/openfcci/fcc-tracking-code-manager
+* Description: A plugin that places quantcast and google analytics tracking code in the footer of each blog on the install (as long as the blog's theme uses the 'wp_footer' action).
+* Version: 1.16.05.12
+* Author: Forum Communications Company
+* Author URI: http://forumcomm.com/
+* Site Wide Only: true
+* Network: true
+*/
 
-	// Add the code to the footer.
-	function tracking_code_footer_listener() {
-		$content = '
-			<!-- Start Quantcast tag -->
-				<script type="text/javascript">
-					_qoptions={qacct:"p-f1gwFa5-x-0VY"};
-				</script>
-				<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
-				<noscript>
-					<div><img src="http://pixel.quantserve.com/pixel/p-f1gwFa5-x-0VY.gif" style="display: none;" class="noBorder" height="1" width="1" alt="Quantcast"/></div>
-				</noscript>
-			<!-- End Quantcast tag -->
-			<!-- Start Google Analytics tag -->
-			<script>
-			(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");
 
-				ga("create", "UA-778232-45", "auto");
-				ga("send", "pageview");
-				</script>
-			<!-- End Google Analytics tag -->
-			';
-		echo $content;
-	}
-	add_action('wp_footer', 'tracking_code_footer_listener', 99); // Add a listener for redoing the adminbar hooks.
+/*--------------------------------------------------------------
+# Quantcast Tracking Pixel & Google Analytics Tracking Tag
+--------------------------------------------------------------*/
+
+/**
+ * Add the code to the footer.
+ */
+function tracking_code_footer_listener() {
+	$content = '
+  <!--  Quantcast Tag -->
+  <script>
+    var ezt = ezt || [];
+    (function() {
+        var elem = document.createElement("script");
+        elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://pixel") + ".quantserve.com/aquant.js?a=p-f1gwFa5-x-0VY";
+        elem.async = true;
+        elem.type = "text/javascript";
+        var scpt = document.getElementsByTagName("script")[0];
+        scpt.parentNode.insertBefore(elem, scpt);
+    }());
+    ezt.push({
+        qacct: "p-f1gwFa5-x-0VY",
+        uid: ""
+    });
+  </script>
+  <noscript>
+    <img src="//pixel.quantserve.com/pixel/p-f1gwFa5-x-0VY.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast"/>
+  </noscript>
+  <!-- End Quantcast Tag -->
+
+	<!-- Start Google Analytics tag -->
+	<script>
+  (function(i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+  ga("create", "UA-778232-45", "auto");
+  ga("send", "pageview");
+		</script>
+	<!-- End Google Analytics tag -->
+		';
+	echo $content;
+}
+add_action('wp_footer', 'tracking_code_footer_listener', 99);
